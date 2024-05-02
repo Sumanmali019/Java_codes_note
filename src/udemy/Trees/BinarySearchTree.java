a702bd5db98a1ae1c4ddaddfdefdfa07ddc4328f
sumanMOMO <sumanmali@cloudcraftz.io>
2024-05-02 18:00:36 +0530

    bfs_dfs

--- src/udemy/Trees/BinarySearchTree.java
+++ src/udemy/Trees/BinarySearchTree.java
@@ -1,12 +1,16 @@
 package udemy.Trees;
 
-public class BinarySearchTree {
-    Node root;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    class Node {
-        int value;
-        Node left;
-        Node right;
+public class BinarySearchTree {
+    public Node root;
+
+    public class Node {
+        public int value;
+        public Node left;
+        public Node right;
 
         Node(int value) {
             this.value = value;
@@ -83,4 +87,81 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    public ArrayList<Integer> BFS() {
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(currentNode);
+
+        while (queue.size() > 0) {
+            currentNode = queue.remove();
+            result.add(currentNode.value);
+
+            if (currentNode.left != null) {
+                queue.add(currentNode.left);
+            }
+            if (currentNode.right != null) {
+                queue.add(currentNode.right);
+            }
+        }
+        return result;
+    }
+
+    public ArrayList<Integer> DFSPreOrder() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        class Travers {
+            Travers(Node currentNode) {
+                result.add(currentNode.value);
+                if (currentNode.left != null) {
+                    new Travers(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Travers(currentNode.right);
+                }
+            }
+        }
+        new Travers(root);
+        return result;
+
+    }
+
+    public ArrayList<Integer> DFSPostOrder() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        class Travers {
+            Travers(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Travers(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Travers(currentNode.right);
+                }
+                result.add(currentNode.value);
+
+            }
+        }
+        new Travers(root);
+        return result;
+    }
+
+    public ArrayList<Integer> DFSInOrder() {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        class Travers {
+            Travers(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Travers(currentNode.left);
+                }
+                result.add(currentNode.value);
+                if (currentNode.right != null) {
+                    new Travers(currentNode.right);
+                }
+            }
+        }
+        new Travers(root);
+        return result;
+    }
+
 }

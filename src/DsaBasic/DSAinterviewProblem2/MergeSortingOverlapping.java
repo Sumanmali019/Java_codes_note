6ebccd759f6e4b8c1a10dfe8fb558a57876bb502
sumanMOMO <sumanmali@cloudcraftz.io>
2024-05-05 20:05:32 +0530

    new_changes

--- src/DsaBasic/DSAinterviewProblem2/MergeSortingOverlapping.java
+++ src/DsaBasic/DSAinterviewProblem2/MergeSortingOverlapping.java
@@ -11,28 +11,63 @@ public class MergeSortingOverlapping {
 
     public int[][] solve(int[][] A) {
 
-        Arrays.sort(A, (a, b) -> a[0] - b[0]);
+        // Arrays.sort(A, (a, b) -> a[0] - b[0]);
 
-        ArrayList<int[]> res = new ArrayList<>();
-        int[] index = A[0];
+        // ArrayList<int[]> res = new ArrayList<>();
+        // int[] index = A[0];
 
-        res.add(index);
+        // res.add(index);
 
-        for (int[] interval : A) {
+        // for (int[] interval : A) {
 
-            int end = index[1];
+        // int end = index[1];
 
-            int next_start = interval[0];
-            int next_end = interval[1];
+        // int next_start = interval[0];
+        // int next_end = interval[1];
 
-            if (end >= next_start) {
-                index[1] = Math.max(end, next_end);
-            } else {
-                index = interval;
-                res.add(index);
+        // if (end >= next_start) {
+        // index[1] = Math.max(end, next_end);
+        // } else {
+        // index = interval;
+        // res.add(index);
+        // }
+        // }
+        // return res.toArray(new int[res.size()][]);
+        // }
+
+        // second approch
+
+        List<int[]> result = new ArrayList<>();
+
+        int n = A.length;
+        int[] current;
+        int i = 0;
+
+        while (i < n) {
+            current = A[i];
+
+            int next[];
+
+            while (i < n) {
+                next = A[i];
+
+                if (isOverLapping(current, next)) {
+                    current[1] = Math.max(current[1], next[1]);
+                    i++;
+                } else {
+                    break;
+                }
             }
+            result.add(current);
         }
-        return res.toArray(new int[res.size()][]);
+        return result.toArray(new int[result.size()][]);
+
     }
 
+    boolean isOverLapping(int[] a1, int[] a2) {
+        if (a1[1] >= a2[1]) {
+            return true;
+        }
+        return false;
+    }
 }
